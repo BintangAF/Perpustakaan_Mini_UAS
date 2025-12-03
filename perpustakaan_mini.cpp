b9aee6d85665150eff13ba796b39739de1076dd0
Bintang AF <kenzixboy@gmail.com>
2025-12-03 19:47:20 +0700

    Menambahkan tampilan menu

--- perpustakaan_mini.cpp
+++ perpustakaan_mini.cpp
@@ -5,7 +5,15 @@ using namespace std;
 
 int main() {
 
-    //
+    cout << "-----Selamat datang di Perpustakaan Mini-----" << endl;
+    cout << "1. Peminjaman Buku" << endl;
+    cout << "2. Pengembalian Buku" << endl;
+    cout << "3. Lihat Daftar Buku" << endl;
+    cout << "4. Cari Buku" << endl;
+    cout << "5. Tambah Buku" << endl;
+    cout << "6. Update Buku" << endl;
+    cout << "7. Hapus Buku" << endl;
+    cout << "8. Exit" << endl;
 
     return 0;
 }
